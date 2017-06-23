429af78d3acd61b05b939a3032b251fb046d4209
Clinton Dreisbach <clinton@dreisbach.us>
2017-06-23 17:07:25 -0400

    Add Sequelize

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,3 +7,19 @@ npm install express --save
 {% if cookiecutter.template_language == "Mustache" %}
 npm install mustache-express --save
 {% endif %}
+
+{% if cookiecutter.use_sequelize == "y" %}
+npm install sequelize --save
+{% endif %}
+
+{% if cookiecutter.database == "PostgreSQL" %}
+npm install --save pg pg-hstore
+{% elif cookiecutter.database == "MySQL" %}
+npm install --save mysql2
+{% elif cookiecutter.database == "SQLite" %}
+$ npm install --save sqlite3
+{% endif %}
+
+{% if cookiecutter.use_sequelize == "y" and cookiecutter.run_sequelize_init == "y" %}
+sequelize init
+{% endif %}
